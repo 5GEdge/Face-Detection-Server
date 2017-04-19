0303637d68ae06b3ac49f19db5a273859548d041
ferraricharles <charles.cjf@gmail.com>
2017-04-19 15:41:31 +0200

    Innovation day used code

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ face detection using haar cascades in remote fashion
 from __future__ import print_function
 import urllib
 import sys
-from flask import Flask, render_template, Response, g
+from flask import Flask, render_template, Response, g,request
 from detect_haar import face_detect
 from cv_decoder import *
 from cv_jpgencoder import *
@@ -16,7 +16,24 @@ from cv_haar_processor import *
 
 app = Flask(__name__)
 
+@app.route('/edge')
+def indexEdge():
+	quality = request.args.get('q')
+   	if quality is None:
+        	quality = 100
+    	return render_template('indexEdge.html', quality=quality)
 
+
+@app.route('/cloud')
+def indexCloud():
+ 	quality = request.args.get('q')
+   	if quality is None:
+        	quality = 100
+    	return render_template('indexCloud.html', quality=quality)
+
+
+
+	
 @app.route('/decode')
 def decode():
     streamurl = str(sys.argv[2])
@@ -49,4 +66,4 @@ def video():
 
 if __name__ == '__main__':
     port = int(sys.argv[1])
-    app.run(port=port, debug=True)
+    app.run(host='169.254.250.163', port=port, debug=True)
